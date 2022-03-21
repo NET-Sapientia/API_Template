647ca83f0cbc782958e989f3dbf2742c004e384f
Osztian <palmarozalia.osztian@gmail.com>
2022-03-21 23:49:29 +0200

    Add request - example

---
+++ API/API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Algorithm> Algorithms { get; set; }
+    }
+}
